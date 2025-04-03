eff3bb1630e8218d8bc45759ed153555b0cee0c1
christian-watson <chischun@gmail.com>
2025-04-03 15:08:39 -0500

    ui changes for sprint 1

--- mudblazor-1-main/MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ mudblazor-1-main/MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec53896f9f51eea85089c3bdc1ff5a5cde0b2f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f165596e4efb4c38d4eafe96a87e1195c265c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
