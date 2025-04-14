be7d4d6545050e6bc81b443821147ce15abaa83d
christian-watson <chischun@gmail.com>
2025-04-14 15:06:03 -0500

    implemented into main file w/ example file

--- mudblazor-1-main/MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
+++ mudblazor-1-main/MyApplication/obj/Debug/net8.0/MyApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc641e053b463febd43dda2f1894087666635c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726ec75e29d35a3f5639be574a882b0be9b975f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
